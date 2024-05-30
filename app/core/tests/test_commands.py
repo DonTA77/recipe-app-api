27b8ed7eba8b31f3cdd0c9c977748aba4b7537fa
DonTA77 <166536839+DonTA77@users.noreply.github.com>
2024-05-30 22:50:45 +0000

    Update test_commands.py

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -14,7 +14,7 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands"""
- 
+
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for dababase to be ready"""
         patched_check.return_value = True
